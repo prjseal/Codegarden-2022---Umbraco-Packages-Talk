ea2ccc6b0c584dd182b606bafe7960cdb81efa6c
Paul Seal <prjseal@gmail.com>
2022-06-12 23:00:28 +0100

    add qr coder sample

---
+++ CGTalk22/QRCoderSample/QRCoderSample/Extensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace QRCoderSample
+{
+    public static class Extensions
+    {
+        public static Byte[] ToArray<TPixel>(this Image image, IImageFormat imageFormat)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                var imageEncoder = image.GetConfiguration().ImageFormatsManager.FindEncoder(imageFormat);
+                image.Save(memoryStream, imageEncoder);
+                return memoryStream.ToArray();
+            }
+        }
+
+        public static System.Drawing.Bitmap ToBitmap(this Image image)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                var imageEncoder = image.GetConfiguration().ImageFormatsManager.FindEncoder(PngFormat.Instance);
+                image.Save(memoryStream, imageEncoder);
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                return new System.Drawing.Bitmap(memoryStream);
+            }
+        }
+
+        public static Image ToImageSharpImage(this System.Drawing.Image bitmap)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                return Image.Load(memoryStream);
+            }
+        }
+
+        public static Image ToImageSharpImage(this System.Drawing.Bitmap bitmap)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                return Image.Load(memoryStream);
+            }
+        }
+    }
+}
---
+++ CGTalk22/QRCoderSample/QRCoderSample/Program.cs
@@ -0,0 +1,40 @@
+﻿using QRCoder;
+using SixLabors.ImageSharp;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Color = System.Drawing.Color;
+using Image = SixLabors.ImageSharp.Image;
+
+namespace QRCoderSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = args[0];
+            var fileName = args[1];
+
+            Console.WriteLine("Url: " + url);
+            Console.WriteLine("FileName: " + fileName + ".png");
+
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q))
+            using (QRCode qrCode = new QRCode(qrCodeData))
+            {
+                var iconAsImage = Bitmap.FromFile("moriyama-square.png");
+                var iconAsSixLaborsImage = iconAsImage.ToImageSharpImage();
+                var iconAsBitmap = iconAsSixLaborsImage.ToBitmap();
+                Bitmap qrCodeImage = qrCode.GetGraphic(20, Color.Black, Color.White, icon: iconAsBitmap);
+                SixLabors.ImageSharp.Image imageToSave = qrCodeImage.ToImageSharpImage();
+
+                ImageConverter converter = new ImageConverter();
+                byte[] qrImageBytes = (byte[])converter.ConvertTo(qrCodeImage, typeof(byte[]));
+
+                System.Drawing.Image image = System.Drawing.Image.FromStream(new MemoryStream(qrImageBytes));
+                image.Save(fileName + ".png", ImageFormat.Png);
+            }
+        }
+    }
+}
